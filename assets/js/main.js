d3cc2b533baa05c5076962bf62791f83943008a6
Damish-N <damishnisal100@gmail.com>
2021-05-27 10:37:16 +0530

    create the svg and home page

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav__link");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+function linkClick() {
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((element) => {
+  element.addEventListener("click", linkClick);
+});
