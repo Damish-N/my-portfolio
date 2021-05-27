b46d518f1e9adcdcc191efb5d878f362a03c86ae
Damish-N <damishnisal100@gmail.com>
2021-05-27 20:35:44 +0530

    click show on the nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,8 +76,31 @@ let swiper = new Swiper(".portfolio__container", {
   },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true, 
+    clickable: true,
   },
   mousewheel: true,
   keyboard: true,
 });
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive(params) {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
